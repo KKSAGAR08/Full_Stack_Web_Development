750ae19fa78dd8181d586a8d7cbb2642a45cc807
KKSAGAR08 <kksagar08062004@gmail.com>
2024-11-07 22:46:56 +0530

    Problmes

--- React/lesson1/src/index.js
+++ React/lesson1/src/index.js
@@ -1,14 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom";
+// import React from "react";
+// import ReactDOM from "react-dom";
 
-const name = "xyz";
-// This will evalute only expression not statement
-// In ES6 we add Templete literals denoted by `${}`
+// const name = "xyz";
+//This will evalute only expression not statement
+//In ES6 we add Templete literals denoted by `${}`
 
-ReactDOM.render(<div>             
-<h1>Hello world </h1>
-<h5>My name is {`${name}`}</h5>      
-<p>My luck number is {Math.floor(Math.random()*10)}</p>
-</div>,
-document.getElementById("root")
-);
+// ReactDOM.render(<div>             
+// <h1>Hello world </h1>
+// <h5>My name is {`${name}`}</h5>      
+// <p>My luck number is {Math.floor(Math.random()*10)}</p>
+// </div>,
+// document.getElementById("root")
+// );
+
+
