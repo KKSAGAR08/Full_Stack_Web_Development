b0c6f6f00d75b7045d38df16782320e8b7079c5e
KKSAGAR08 <kksagar08062004@gmail.com>
2024-11-08 01:01:09 +0530

    React

--- React/lesson1/src/project_2.js
+++ React/lesson1/src/project_2.js
@@ -1,14 +1,12 @@
 import React from "react";
-import "./Image_food.css";
 
+const image = "https://picsum.photos/200/300";
 function Element(){
     return(
         <div>
-            <h1>Favorite Food</h1>
+            <h1>Random Images</h1>
             <div>
-                <img src="https://static.vecteezy.com/system/resources/previews/015/933/115/non_2x/chole-bhature-is-a-north-indian-food-dish-a-combination-of-chana-masala-and-bhatura-or-puri-free-photo.jpg"></img>
-                <img src="https://www.shutterstock.com/image-photo/fried-salmon-steak-cooked-green-600nw-2489026949.jpg"></img>
-                <img src="https://media.cnn.com/api/v1/images/stellar/prod/140430115517-06-comfort-foods.jpg?q=x_0,y_0,h_720,w_1280,c_fill/w_800"></img>
+            <img src={image + "?grayscale"} alt="Photo"></img>
             </div>
         </div>
     )
