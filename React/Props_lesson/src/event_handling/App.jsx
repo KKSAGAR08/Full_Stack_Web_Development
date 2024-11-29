220559fa5719fdeb865f793e8c161db4e7fad5bf
KKSAGAR08 <kksagar08062004@gmail.com>
2024-11-29 19:58:35 +0530

    React

--- React/Props_lesson/src/event_handling/App.jsx
+++ React/Props_lesson/src/event_handling/App.jsx
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import "./style.css"
 
 function App() {
+const [beforeClick, afterClick] = useState("Hello")
+const [isMouseOver,setMouseOver] = useState(false)
+
+function buttonClicked(){
+    afterClick("Submitted");
+}
+function styleChanged(){
+    setMouseOver(true)
+}
+function styleComback(){
+    setMouseOver(false)
+}
+
+
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>{beforeClick}</h1>
       <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <button style={{backgroundColor: isMouseOver?"black":"white"}} 
+      onMouseOver={styleChanged} 
+      onClick={buttonClicked}
+      onMouseOut={styleComback}
+      >Submit</button>
     </div>
   );
 }
